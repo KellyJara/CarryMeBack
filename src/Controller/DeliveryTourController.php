d983dd13002c41fcf29e8566e6c0261b52c27db4
KellyJara <vaneorozco24@hotmail.com>
2024-11-04 22:57:54 +0100

    serialize 1

--- src/Controller/DeliveryTourController.php
+++ src/Controller/DeliveryTourController.php
@@ -3,38 +3,57 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\DeliveryTour;
 use App\Repository\DeliveryTourRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use DateTimeImmutable ;
 
 
 #[Route('api/deliverytour', name: 'app_api_deliverytour_')]
 class DeliveryTourController extends AbstractController
 {
-  public function __construct(private EntityManagerInterface $manager, private DeliveryTourRepository $repository)
+  public function __construct(
+    private EntityManagerInterface $manager, 
+    private DeliveryTourRepository $repository,
+    private SerializerInterface $serializer,
+    private UrlGeneratorInterface $urlGenerator
+    )
 {
   
 }
-    #[Route(('/'), name:'new', methods: 'POST')]
-    public function new(): JsonResponse
+    #[Route(methods: 'POST')]
+    public function new(Request $request): JsonResponse
     {
-      $deliverytour= new DeliveryTour();
+      $deliverytour= $this->serializer->deserialize($request->getContent(), DeliveryTour::class,'json');
+      $deliverytour= setCreatedAt(new DateTimeImmutable());
+
+      /*$deliverytour= new DeliveryTour();
       $deliverytour= setOrigin('Paris');
       $deliverytour= setDestination('Bordeaux');
       $deliverytour= setMeansOfTransport('Plane');
-      $deliverytour= setDescription('I have some space on my suitcase so I can carry a parsel for you');
-      $deliverytour= setCreatedAt(new DateTimeImmutable());
+      $deliverytour= setDescription('I have some space on my suitcase so I can carry a parsel for you');*/
+      
 
       // Tell Doctrine you want to (eventually) save the deliverytour (no queries yet)
       $this->manager->persist($deliverytour);
       // Actually executes the queries (i.e. the INSERT query)
       $this->manager->flush(); 
        // A stocker en base de donnees
-     return $this->json(['message' => "New DeliveryTour created with {$deliverytour->getId()} id"],
-     Response::HTTP_CREATED, );
+
+       $responseData = $this->serializer->serialize($deliverytour, 'json');
+       $location= $this->urlGenerator->generate(
+        'app_api_restaurant_show',
+        ['id'=>$deliverytour->getId()],
+        UrlGeneratorInterface::ABSOLUTE_URL,
+       );
+     return new JsonResponse($responseData, 
+     Response::HTTP_CREATED,["Location"=>$location],true);
     }
     
 
